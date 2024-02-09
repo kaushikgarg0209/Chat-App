fcdcfb33e406df204200519223a69b7bd13e6c22
kaushikgarg0209 <kaushikgarg0209@gmail.com>
2024-02-09 12:25:42 +0530

    host to env

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+const Host = import.meta.env.VITE_HOST
+export const host = Host;
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
