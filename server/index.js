81bb6edbdae248376ab2593d20ee66d0455fa0e3
kaushikgarg0209 <kaushikgarg0209@gmail.com>
2024-02-09 13:49:04 +0530

    added deployed frontend link

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-app-three-nu.vercel.app/' || 'http://localhost:5173',
     credentials: true,
   },
 });
