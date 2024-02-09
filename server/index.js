d393c9dc9edae59fbeb181b19c4379ab58390dcb
kaushikgarg0209 <kaushikgarg0209@gmail.com>
2024-02-09 14:31:15 +0530

    front end url

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: true,
+    origin:"https://chat-app-three-nu.vercel.app/",
     credentials: true,
   },
 });
